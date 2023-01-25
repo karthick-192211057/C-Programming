d459ca568a83901036d45c495b41b56dcdf22ff3
karthick-192211057 <123526175+karthick-192211057@users.noreply.github.com>
2023-01-25 10:20:29 +0530

    Add files via upload

---
+++ number series.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>    
+#include<conio.h>
+int main()    
+{    
+ int i,n;    
+ printf("Enter the number of elements:");    
+ scanf("%d",&n);    
+ for(i=1;i<=n;i++)    
+ {    
+  printf("%d\n",i);
+ }  
+  return 0;}
