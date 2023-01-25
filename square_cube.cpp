422e3d5b19a06566dff1a4370429eab22a87e17f
karthick-192211057 <123526175+karthick-192211057@users.noreply.github.com>
2023-01-25 10:02:34 +0530

    Add files via upload

---
+++ square_cube.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	float s,c,n;
+	printf("enter a value");
+	scanf("%f",&n);
+	s=n*n;
+	printf("square of the number: %0.2f\n",s);
+	c=n*n*n;
+	printf("cube of the number:%0.2f\n",c);
+	
+}
