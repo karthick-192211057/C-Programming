f5dd436e4dd4533c98a7aed0a23788f89bc9715b
karthick-192211057 <123526175+karthick-192211057@users.noreply.github.com>
2023-01-27 08:51:35 +0530

    Add files via upload

---
+++ leap year.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+main(){
+int i,j,date,month,year,c=0;
+    printf("Enter date (dd/mm/yy): ");
+    scanf("%d/%d/%d", &date,&month,&year);
+	if(date<0 || date>31){
+		printf("Invalid input...");
+	}
+	else if(month<0 || month>12){
+		printf("invalid input...");
+	}
+	else if(year<0){
+		printf("invalid input...");
+	}
+	else{
+		if (year % 400 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else if (year % 100 == 0) {
+      printf("%d is not a leap year.", year);
+   }
+   else if (year % 4 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   else {
+      printf("%d is not a leap year.", year);
+   }
+	}
+}
+
